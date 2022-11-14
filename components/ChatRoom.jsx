5a502ea05d902b75fffdc6fc5dacd85783eaa331
lhs0698 <104623873+lhs0698@users.noreply.github.com>
2022-11-14 18:37:14 +0900

    chatroom UI

--- components/ChatRoom.jsx
+++ components/ChatRoom.jsx
@@ -1,16 +1,8 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, View } from "react-native";
 
-import {
-  NativeBaseProvider,
-  Input,
-  Button,
-  useBreakpointValue,
-  AddIcon,
-  Box,
-  FlatList,
-  VStack,
-} from "native-base";
+import { NativeBaseProvider, Input, IconButton, Box, Icon } from "native-base";
+import { Entypo } from "@expo/vector-icons";
 
 export default function ChatRoom() {
   return (
@@ -18,7 +10,70 @@ export default function ChatRoom() {
       <SafeAreaView style={styles.container}>
         <View style={styles.viewTest}></View>
         <View style={styles.foot}>
-          <Input variant="rounded" w="250px" style={styles.InputTest}></Input>
+          <Box flexDirection="row" p="5" alignItems="center">
+            <IconButton
+              icon={<Icon as={Entypo} name="plus" />}
+              borderRadius="full"
+              _icon={{
+                color: "blue.500",
+                size: "md",
+              }}
+              _hover={{
+                bg: "yellow.400:alpha.20",
+              }}
+              _pressed={{
+                bg: "yellow.300:alpha.30",
+                _icon: {
+                  name: "plus",
+                },
+                _ios: {
+                  _icon: {
+                    size: "2xl",
+                  },
+                },
+              }}
+              _ios={{
+                _icon: {
+                  size: "2xl",
+                },
+              }}
+            />
+            <Input
+              variant="rounded"
+              bg="white"
+              w="250"
+              h="10"
+              ml="3"
+              mr="3"
+            ></Input>
+            <IconButton
+              icon={<Icon as={Entypo} name="paper-plane" />}
+              borderRadius="full"
+              _icon={{
+                color: "blue.500",
+                size: "md",
+              }}
+              _hover={{
+                bg: "yellow.400:alpha.20",
+              }}
+              _pressed={{
+                bg: "yellow.300:alpha.30",
+                _icon: {
+                  name: "paper-plane",
+                },
+                _ios: {
+                  _icon: {
+                    size: "2xl",
+                  },
+                },
+              }}
+              _ios={{
+                _icon: {
+                  size: "2xl",
+                },
+              }}
+            />
+          </Box>
         </View>
       </SafeAreaView>
     </NativeBaseProvider>
@@ -33,15 +88,10 @@ const styles = StyleSheet.create({
   viewTest: {
     width: "100%",
     height: "90%",
-    backgroundColor: "#CCCCFF",
+    backgroundColor: "#FFF0F0",
   },
   foot: {
-    backgroundColor: "CC9966",
-    height: "100%",
-    alignItems: "center",
-    marginTop: 15,
-  },
-  InputTest: {
     backgroundColor: "white",
+    alignItems: "center",
   },
 });
