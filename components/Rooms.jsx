c6bfa8b17245d4226288660487efb7de0690ea13
lhs0698 <yohs0698@naver.com>
2022-12-07 01:54:28 +0900

    Rooms design up

--- components/Rooms.jsx
+++ components/Rooms.jsx
@@ -10,6 +10,9 @@ import {
   Icon,
   Box,
   IconButton,
+  HStack,
+  Spinner,
+  Center
 } from "native-base";
 import { AntDesign, Entypo } from "@expo/vector-icons";
 import { db } from "../firebase_config";
@@ -17,22 +20,13 @@ import { db } from "../firebase_config";
 import {
   addDoc,
   collection,
-  doc,
-  getDoc,
   getDocs,
   where,
   query,
   deleteDoc,
-  onSnapshot,
-  Firestore,
-  setDoc,
 } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { v4 as uuidv4 } from "uuid"; // id의 고유한 값을 주기위한 라이브러리 uuid
-import { async } from "@firebase/util";
-
-// import { doc, setDoc, Timestamp } from "firebase/firestore";
-// import { getFirestore } from "firebase/firestore";
 
 export default function Rooms() {
   const roomRef = collection(db, "Rooms");
@@ -40,13 +34,13 @@ export default function Rooms() {
   const [roomName, setRoomName] = useState("");
   const [roomList, setRoomList] = useState([]);
   const [loginEmail, setLoginEmail] = useState();
-  const [list, setList] = useState();
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
 
   const inputChange = (text) => {
     setRoomName(text);
   };
 
+  // 현재 로그인한 사용자 가져오기
   const auth = getAuth();
   onAuthStateChanged(auth, (user) => {
     if (user) {
@@ -54,20 +48,18 @@ export default function Rooms() {
     }
   });
 
+  // 방 추가하기
   const addRoom = async () => {
     try {
-      // 데이터 추가
       const newRoomName = roomName;
       const roomUUID = uuidv4();
-
+      // 데이터 추가
       await addDoc(collection(db, "Rooms"), {
         name: newRoomName,
         id: roomUUID,
         createdAt: new Date().toString(),
         creator: loginEmail,
       });
-
-      
       // 새로생성한 방 데이터 가져오기
       const q = query(roomRef, where("id", "==", roomUUID));
       // collection의 문서를 query하여 여러문서를 검색한다, where()을 사용해서 특정 조건을 충족하는 문서를 가져온다. ex) id와 roomUUID와 같으면 가져온다.
@@ -85,72 +77,48 @@ export default function Rooms() {
     }
   };
 
-
+  // firestore list Up
   useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
     const getRoomList = async () => {
       const initRoomList = [];
 
-      
-      // 새로생성한 방 데이터 가져오기
       const q = query(roomRef);
-
       const querySnapshot = await getDocs(q);
       querySnapshot.forEach((doc) => {
-        initRoomList.push(doc.data())
-      })
+        initRoomList.push(doc.data());
+      });
 
-      setRoomList(state => [ ...state, ...initRoomList ])
-      setIsLoading(false)
-    }
-    getRoomList()
-  },[])
+      setRoomList((state) => [...state, ...initRoomList]);
+      setIsLoading(false);
+    };
+    getRoomList();
+  }, []);
 
-  // const listUp = async () => {
-  //   const querySnapshot = await getDocs(collection(db, "Rooms"));
-  //   querySnapshot.forEach((doc) => {
-  //     // doc.data() is never undefined for query doc snapshots
-  //     console.log(doc.id, " => ", doc.data());
-  //     roomList.map((eachroom) => {
-  //       return (
-  //         <View style={styles.container} key={eachroom.id}>
-  //           <Text>{eachroom.name}</Text>
-  //           <Box w="30px">
-  //             <IconButton
-  //               icon={<Icon as={Entypo} name="trash" />}
-  //               // onPress={roomDelete}
-  //             />
-  //           </Box>
-  //         </View>
-  //       );
-  //     });
-  //   });
-  // };
-  if(isLoading)
- return <View style={styles.loadingCotainer}><Text>로딩중......</Text></View>
+  // 채팅 list를 가져오는 동안 보여지는 로딩화면
+  if (isLoading)
+    return (
+      <NativeBaseProvider>
+        <Center flex={1}>
+          <HStack space={8} display="flex" justifyContent="center">
+            <Spinner color="indigo.500" size="lg"/>
+          </HStack>
+        </Center>
+      </NativeBaseProvider>
+    );
 
   return (
     <NativeBaseProvider>
-      {/* {roomList.map((eachroom) => {
-        return (
-          <View style={styles.container} key={eachroom.id}>
-            <Text>{eachroom.name}</Text>
-            <Box w="30px">
-              <IconButton
-                icon={<Icon as={Entypo} name="trash" />}
-                // onPress={roomDelete}
-              />
-            </Box>
-          </View>
-        );
-      })} */}
       <View>
-        {
-        roomList.map((eachroom) => {
+        {roomList.map((eachroom) => {
           return (
             <View style={styles.container} key={eachroom.id}>
-              <Text>{eachroom.name}</Text>
-              <Box w="30px">
+              <Text style={styles.texts}>{eachroom.name}</Text>
+              <Box style={styles.icon}>
+                <IconButton
+                  icon={<Icon as={Entypo} name="pencil" />}
+                  // onPress={roomDelete}
+                />
                 <IconButton
                   icon={<Icon as={Entypo} name="trash" />}
                   // onPress={roomDelete}
@@ -207,16 +175,50 @@ export default function Rooms() {
 const styles = StyleSheet.create({
   container: {
     width: "100%",
-    height: 70,
-    backgroundColor: "#feff",
-    border: "solid",
+    height: 60,
+    backgroundColor: "#eeff",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10
   },
-
   loadingCotainer: {
-    width: '100%',
-    height: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  texts: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: 20
+  },
+  icon: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
   }
 });
+
+// const listUp = async () => {
+//   const querySnapshot = await getDocs(collection(db, "Rooms"));
+//   querySnapshot.forEach((doc) => {
+//     // doc.data() is never undefined for query doc snapshots
+//     console.log(doc.id, " => ", doc.data());
+//     roomList.map((eachroom) => {
+//       return (
+//         <View style={styles.container} key={eachroom.id}>
+//           <Text>{eachroom.name}</Text>
+//           <Box w="30px">
+//             <IconButton
+//               icon={<Icon as={Entypo} name="trash" />}
+//               // onPress={roomDelete}
+//             />
+//           </Box>
+//         </View>
+//       );
+//     });
+//   });
+// };
